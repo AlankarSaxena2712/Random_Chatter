6688a950dd8a38b357340887d05f8c16a5f36fed
AlankarSaxena2712 <saxenaalankar42@gmail.com>
2020-08-14 21:53:08 +0530

    Some changes in chat feature

--- chat/views.py
+++ chat/views.py
@@ -14,14 +14,19 @@ from .serializers import MessageSerializer
 @csrf_exempt
 def message_list(request, sender=None, receiver=None):
     if request.method == 'GET':
-        messages = Message.objects.filter(sender_id = sender, receiver_id = receiver)
+        messages_1 = Message.objects.filter(sender_id=sender, receiver_id=receiver)
+        messages_2 = Message.objects.filter(sender_id=receiver, receiver_id=sender)
+        messages = messages_1 | messages_2
         serializer = MessageSerializer(messages, many = True)
         return Response(serializer.data)
 
     if request.method == 'POST':
-        # data = JSONParser().parse(request)
         serializer = MessageSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
+            messages_1 = Message.objects.filter(sender_id=sender, receiver_id=receiver)
+            messages_2 = Message.objects.filter(sender_id=receiver, receiver_id=sender)
+            messages = messages_1 | messages_2
+            serializer = MessageSerializer(messages, many = True)
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status = 400)
